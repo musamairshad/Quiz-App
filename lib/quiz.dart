aa20414bc3da0dfd299d851663d9b26b3f2e35ec
Muhammad Usama <imusamairshad@gmail.com>
2023-05-09 23:14:19 +0500

    quiz.dart file added.

---
+++ lib/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromARGB(255, 78, 13, 151),
+                    Color.fromARGB(255, 107, 15, 168),
+                  ]),
+            ),
+            child: const StartScreen()),
+      ),
+    );
+  }
+}
