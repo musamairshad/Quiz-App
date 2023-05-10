6ed3a585b626b89f2acf61710333358e812c3497
Muhammad Usama <imusamairshad@gmail.com>
2023-05-10 12:06:50 +0500

    modified quiz.dart file.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,22 +12,31 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = StartScreen(switchScreen);
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color.fromARGB(255, 78, 13, 151),
-                    Color.fromARGB(255, 107, 15, 168),
-                  ]),
-            ),
-            child: const StartScreen()),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168),
+                ]),
+          ),
+          child: activeScreen,
+        ),
       ),
     );
   }
