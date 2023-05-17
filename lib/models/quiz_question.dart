e5d14fc99557564ee699668a5a6661f221d1e47d
Muhammad Usama <imusamairshad@gmail.com>
2023-05-17 11:28:55 +0500

    quiz_question.dart file is modified.

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final newShuffledList = List.of(answers);
+    newShuffledList.shuffle();
+    return newShuffledList;
+  }
 }
