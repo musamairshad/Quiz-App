3c504cc07694ca3b8fec30ac57eacaa1025ec0a8
Muhammad Usama <imusamairshad@gmail.com>
2023-05-17 14:12:32 +0500

    start_screen.dart file is modified.

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -24,12 +25,13 @@ class StartScreen extends StatelessWidget {
           //   ),
           // ),
           const SizedBox(height: 80.0),
-          const Text(
+          Text(
             'Learn Flutter the fun way!',
-            style: TextStyle(
-              color: Color.fromARGB(255, 237, 223, 252),
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 237, 223, 252),
               fontSize: 24.0,
             ),
+        
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
