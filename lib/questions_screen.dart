5aeb1c742d1b5036e94093e0b8faef7ce7bb0cb9
Muhammad Usama <imusamairshad@gmail.com>
2023-05-17 11:29:16 +0500

    questions_screen.dart file is modified.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,27 +18,36 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer){
-            return AnswerButton(answerText: answer,
-            onTap: (){},
-            );
-          }),
-          // AnswerButton(
-          //   answerText: currentQuestion.answers[0],
-          //   onTap: () {},
-          // ),
-        ],
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            }),
+
+            // ...currentQuestion.answers().map() => previously it is like
+            // this.
+
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[0],
+            //   onTap: () {},
+            // ),
+          ],
+        ),
       ),
     );
   }
