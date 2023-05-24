fcd362bb0a7a6cc5489f86d1d7d4e1839f6bb5ac
Muhammad Usama <imusamairshad@gmail.com>
2023-05-24 12:24:00 +0500

    questions_summary.dart file is added.

---
+++ lib/questions_summary.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: summaryData.map((data) {
+        return Row(
+          children: [
+            Text(((data['question_index'] as int) + 1).toString()),
+            Expanded(
+              // Here we wrap the Column Widget with an Expanded Widget
+              // to avoid RenderFlex issue.
+              child: Column(
+                children: [
+                  Text(data['question'] as String),
+                  const SizedBox(height: 5),
+                  Text(data['user_answer'] as String),
+                  Text(data['correct_answer'] as String),
+                ],
+              ),
+            ),
+          ],
+        );
+      }).toList(),
+    );
+  }
+}
