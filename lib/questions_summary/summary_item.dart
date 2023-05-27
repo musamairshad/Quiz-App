f7ff5811f7d912637b1c9d3fa23aa407f410f0f2
Muhammad Usama <imusamairshad@gmail.com>
2023-05-27 13:58:23 +0500

    summary_item.dart file is added.

--- lib/questions_summary/summary_item.dart
+++ lib/questions_summary/summary_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/questions_summary/question_identifier.dart';
 
+
 class SummaryItem extends StatelessWidget {
   const SummaryItem(this.itemData, {super.key});
 
