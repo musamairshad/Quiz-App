428ed1406c4c5e0ce574a4f7d432af83e34b26f6
Muhammad Usama <imusamairshad@gmail.com>
2023-05-27 20:42:21 +0500

    results_screen.dart file is modified.

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -10,7 +10,8 @@ class ResultsScreen extends StatelessWidget {
   final List<String> chosenAnswers;
   final void Function() onRestart;
 
-  List<Map<String, Object>> getSummaryData() {
+  // List<Map<String, Object>> summaryData() {
+  List<Map<String, Object>> get summaryData {
     final List<Map<String, Object>> summary = [];
 
     for (var i = 0; i < chosenAnswers.length; i++) {
@@ -29,11 +30,16 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final summaryData = getSummaryData();
+    // final summaryData = getSummaryData();
+    // final summaryData = summaryData; // Here the name duplication thing occurs.
+    // summaryData on the right is the getter basically.
     final numOfTotalQuestions = questions.length;
-    final numOfCorrectQuestions = summaryData.where((data) {
-      return data['user_answer'] == data['correct_answer'];
-    }).length;
+    final numOfCorrectQuestions = summaryData
+        .where(
+          (data) => data['user_answer'] == data['correct_answer'],
+          // return data['user_answer'] == data['correct_answer'];
+        )
+        .length;
 
     return SizedBox(
         width: double.infinity,
